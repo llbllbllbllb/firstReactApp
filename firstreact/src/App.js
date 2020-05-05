8069c94783565ff446cf21bed427f905eb0e9090
llbllbllbllb <295319725@qq.com>
2020-05-05 12:10:00 -0400

    finished

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Tweet from './tweet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+// app.js return a componet
+
+// create a component by writing functions
+function App(){
+
+  const [users, setUsers] = useState([
+    {name:'ed', message:'hello there'},
+    {name:'john', message:'i am john snow'},
+    {name:'traversy', message:'i am awesome'}
+  ]);
+
+
+
+
+  return(
+    <div className="app">
+      {users.map(user =>(
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
+
 }
+
+
+
 
 export default App;
